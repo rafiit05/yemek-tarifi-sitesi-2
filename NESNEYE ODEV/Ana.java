2081c1fe6a3463549eb66e6810a90ed49919bba8
Rafiya <rafiyazhorabekova05@gmail.com>
2025-06-02 21:53:51 +0600

    read.me

---
+++ NESNEYE ODEV/Ana.java
@@ -0,0 +1,49 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+interface Observer {
+    void update(String message);
+}
+
+class ConcreteObserver implements Observer {
+    private String name;
+
+    public ConcreteObserver(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String message) {
+        System.out.println(name + " mesaj aldı: " + message);
+    }
+}
+
+class Subject {
+    private List<Observer> observers = new ArrayList<>();
+
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+        Observer obs1 = new ConcreteObserver("Observer1");
+        Observer obs2 = new ConcreteObserver("Observer2");
+
+        subject.addObserver(obs1);
+        subject.addObserver(obs2);
+        subject.notifyObservers("Merhaba Dünya!");
+    }
+}
